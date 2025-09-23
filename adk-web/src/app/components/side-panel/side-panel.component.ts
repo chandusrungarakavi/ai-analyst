c7d048f39a480134f762c1b768ed9a1117290d6a
chandu.dummy98 <chandu.dummy98@gmail.com>
2025-09-23 08:05:29 +0000

    ui changes

--- adk-web/src/app/components/side-panel/side-panel.component.ts
+++ adk-web/src/app/components/side-panel/side-panel.component.ts
@@ -1,43 +1,22 @@
-/**
- * @license
- * Copyright 2025 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import { Component, EventEmitter, inject, Input, Output, viewChild } from '@angular/core';
+import { MatMiniFabButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTab, MatTabGroup, MatTabLabel } from '@angular/material/tabs';
+import { MatTooltip } from '@angular/material/tooltip';
+import { SafeHtml } from '@angular/platform-browser';
+import { NgxJsonViewerModule } from 'ngx-json-viewer';
+import { EvalCase } from '../../core/models/Eval';
+import { Session } from '../../core/models/Session';
+import { ArtifactTabComponent } from '../artifact-tab/artifact-tab.component';
+import { EvalTabComponent } from '../eval-tab/eval-tab.component';
+import { EventTabComponent } from '../event-tab/event-tab.component';
+import { SessionTabComponent } from '../session-tab/session-tab.component';
+import { StateTabComponent } from '../state-tab/state-tab.component';
+import { TraceTabComponent } from '../trace-tab/trace-tab.component';
+import { MetricsPanelComponent } from '../metrics-panel/metrics-panel.component';
+import { SidePanelMessagesInjectionToken } from './side-panel.component.i18n';
 
-import {Component, EventEmitter, inject, Input, Output, viewChild} from '@angular/core';
-import {MatMiniFabButton} from '@angular/material/button';
-import {MatIcon} from '@angular/material/icon';
-import {MatPaginator, PageEvent} from '@angular/material/paginator';
-import {MatTab, MatTabGroup, MatTabLabel} from '@angular/material/tabs';
-import {MatTooltip} from '@angular/material/tooltip';
-import {SafeHtml} from '@angular/platform-browser';
-import {NgxJsonViewerModule} from 'ngx-json-viewer';
-
-import {EvalCase} from '../../core/models/Eval';
-import {Session} from '../../core/models/Session';
-import {ArtifactTabComponent} from '../artifact-tab/artifact-tab.component';
-import {EvalTabComponent} from '../eval-tab/eval-tab.component';
-import {EventTabComponent} from '../event-tab/event-tab.component';
-import {SessionTabComponent} from '../session-tab/session-tab.component';
-import {StateTabComponent} from '../state-tab/state-tab.component';
-import {TraceTabComponent} from '../trace-tab/trace-tab.component';
-
-import {SidePanelMessagesInjectionToken} from './side-panel.component.i18n';
-
-/**
- * Side panel component.
- */
 @Component({
   selector: 'app-side-panel',
   templateUrl: './side-panel.component.html',
@@ -58,6 +37,7 @@ import {SidePanelMessagesInjectionToken} from './side-panel.component.i18n';
     MatMiniFabButton,
     MatIcon,
     NgxJsonViewerModule,
+    MetricsPanelComponent
   ],
 })
 export class SidePanelComponent {
@@ -90,6 +70,7 @@ export class SidePanelComponent {
   @Output() readonly page = new EventEmitter<PageEvent>();
   @Output() readonly closeSelectedEvent = new EventEmitter<void>();
   @Output() readonly openImageDialog = new EventEmitter<string|null>();
+  @Output() readonly metricsChanged = new EventEmitter<any>();
 
   readonly eventTabComponent = viewChild(EventTabComponent);
   readonly sessionTabComponent = viewChild(SessionTabComponent);
